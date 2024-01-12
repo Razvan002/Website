71721607154ce9a01b9a170f32a346c5abb1da93
Razvan <gradea.razvan03@gmail.com>
2024-01-12 15:36:31 +0200

    login and register commit

--- index.js
+++ index.js
@@ -1,15 +1,18 @@
 function openLoginForm() {
-    document.getElementById('login-modal').style.display = 'block';
-    document.getElementById('overlay').style.display = 'block';
+    document.getElementById('login-btn').style.display = 'block';
+    document.getElementById('login-popup').style.display = 'block';
+    document.getElementById('close-btn-login').style.display = 'block';
 }
 
 function openSignupForm() {
-    document.getElementById('signup-modal').style.display = 'block';
-    document.getElementById('overlay').style.display = 'block';
+    document.getElementById('signup-btn').style.display = 'block';
+    document.getElementById('register-popup').style.display = 'block';
+    document.getElementById('close-btn-register').style.display = 'block';
 }
 
 function closeModals() {
-    document.getElementById('login-modal').style.display = 'none';
-    document.getElementById('signup-modal').style.display = 'none';
-    document.getElementById('overlay').style.display = 'none';
+    document.getElementById('close-btn-login').style.display = 'none';
+    document.getElementById('close-btn-register').style.display = 'none';
+    document.getElementById('login-popup').style.display = 'none';
+    document.getElementById('register-popup').style.display = 'none';
 }
